96b1b1d1230447ba9e49250fa41bca902238f2b9
PrashantAhir <103811881+PrashantAhir@users.noreply.github.com>
2022-05-14 12:16:33 +0530

    Add files via upload

--- Hello1.java
+++ Hello1.java
@@ -1,8 +1,12 @@
 public class Hello1 {
 
-    public static  void  main(String [] args ){
+    public static  void  main(String [] args ) {
+
+        for (int i = 1; i <= 10; i++) {
 
 
-        System.out.println("Hello World");
+            System.out.println("Hello World..." + i);
+
+        }
     }
 }
